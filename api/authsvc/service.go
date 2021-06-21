sixhills/shellhub

api: Add support for API token
Also add apierr package to handle the errors
generated by the api.

Signed-off-by: asakiz <asakizin@gmail.com>

api/authsvc/service.go
@@ -30,6 +30,7 @@ type Service interface {
 	AuthGetToken(ctx context.Context, tenant string) (*models.UserAuthResponse, error)
 	AuthPublicKey(ctx context.Context, req *models.PublicKeyAuthRequest) (*models.PublicKeyAuthResponse, error)
 	AuthSwapToken(ctx context.Context, ID, tenant string) (*models.UserAuthResponse, error)
+	AuthToken(ctx context.Context, req *models.TokenAuthRequest) (*models.TokenAuthResponse, error)
 	PublicKey() *rsa.PublicKey
 }
 
@@ -274,6 +275,36 @@ func (s *service) AuthSwapToken(ctx context.Context, username, tenant string) (*
 	}
 
 	return nil, nil
+}
+
+func (s *service) AuthToken(ctx context.Context, req *models.TokenAuthRequest) (*models.TokenAuthResponse, error) {
+	id := sha256.Sum256(structhash.Dump(req.Name, 1))
+
+	namespace, err := s.store.GetNamespace(ctx, req.Name)
+	if err != nil && err != store.ErrNamespaceNoDocuments {
+		return nil, err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, models.TokenAuthClaims{
+		ID:       hex.EncodeToString(id[:]),
+		TenantID: namespace.TenantID,
+		AuthClaims: models.AuthClaims{
+			Claims: "token",
+		},
+	})
+
+	tokenStr, err := token.SignedString(s.privKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.TokenAuthResponse{
+		ID:        hex.EncodeToString(id[:]),
+		Token:     tokenStr,
+		TenantID:  namespace.TenantID,
+		ReadOnly:  true,
+		Namespace: namespace.Name,
+	}, nil
 }
 
 func (s *service) PublicKey() *rsa.PublicKey {

api/store/mocks/store.go
@@ -1,4 +1,4 @@
-// Code generated by mockery v1.0.0. DO NOT EDIT.
+// Code generated by mockery v0.0.0-dev. DO NOT EDIT.
 
 package mocks
 
@@ -51,6 +51,20 @@ func (_m *Store) AddNamespaceUser(ctx context.Context, namespace string, ID stri
 	}
 
 	return r0, r1
+}
+
+// ChangePermission provides a mock function with given fields: ctx, tokenID
+func (_m *Store) ChangePermission(ctx context.Context, tokenID string) error {
+	ret := _m.Called(ctx, tokenID)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string) error); ok {
+		r0 = rf(ctx, tokenID)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
 }
 
 // CreateFirewallRule provides a mock function with given fields: ctx, rule
@@ -141,6 +155,29 @@ func (_m *Store) CreateSession(ctx context.Context, session models.Session) (*mo
 	return r0, r1
 }
 
+// CreateToken provides a mock function with given fields: ctx, namespace, token
+func (_m *Store) CreateToken(ctx context.Context, namespace string, token *models.Token) (*models.Token, error) {
+	ret := _m.Called(ctx, namespace, token)
+
+	var r0 *models.Token
+	if rf, ok := ret.Get(0).(func(context.Context, string, *models.Token) *models.Token); ok {
+		r0 = rf(ctx, namespace, token)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*models.Token)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, string, *models.Token) error); ok {
+		r1 = rf(ctx, namespace, token)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // CreateUser provides a mock function with given fields: ctx, user
 func (_m *Store) CreateUser(ctx context.Context, user *models.User) error {
 	ret := _m.Called(ctx, user)
@@ -218,6 +255,20 @@ func (_m *Store) DeletePublicKey(ctx context.Context, fingerprint string, tenant
 	var r0 error
 	if rf, ok := ret.Get(0).(func(context.Context, string, string) error); ok {
 		r0 = rf(ctx, fingerprint, tenant)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// DeleteToken provides a mock function with given fields: ctx, namespace
+func (_m *Store) DeleteToken(ctx context.Context, namespace string) error {
+	ret := _m.Called(ctx, namespace)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string) error); ok {
+		r0 = rf(ctx, namespace)
 	} else {
 		r0 = ret.Error(0)
 	}
@@ -582,6 +633,29 @@ func (_m *Store) GetStats(ctx context.Context) (*models.Stats, error) {
 	var r1 error
 	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
 		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// GetToken provides a mock function with given fields: ctx, namespace
+func (_m *Store) GetToken(ctx context.Context, namespace string) (*models.Token, error) {
+	ret := _m.Called(ctx, namespace)
+
+	var r0 *models.Token
+	if rf, ok := ret.Get(0).(func(context.Context, string) *models.Token); ok {
+		r0 = rf(ctx, namespace)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*models.Token)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, namespace)
 	} else {
 		r1 = ret.Error(1)
 	}

gustavosbarreto/docbuilder

api/authsvc/service.go
@@ -21,6 +21,7 @@ import (
 	"github.com/shellhub-io/shellhub/api/store"
 	"github.com/shellhub-io/shellhub/pkg/clock"
 	"github.com/shellhub-io/shellhub/pkg/models"
+	"golang.org/x/crypto/bcrypt"
 	"gopkg.in/go-playground/validator.v9"
 )
 
@@ -33,6 +34,7 @@ type Service interface {
 	AuthPublicKey(ctx context.Context, req *models.PublicKeyAuthRequest) (*models.PublicKeyAuthResponse, error)
 	AuthSwapToken(ctx context.Context, ID, tenant string) (*models.UserAuthResponse, error)
 	AuthUserInfo(ctx context.Context, username, tenant, token string) (*models.UserAuthResponse, error)
+	AuthAPIToken(ctx context.Context, req *models.APITokenAuthRequest) (*models.APITokenAuthResponse, error)
 	PublicKey() *rsa.PublicKey
 }
 
@@ -310,6 +312,43 @@ func (s *service) AuthUserInfo(ctx context.Context, username, tenant, token stri
 		Tenant: tenant,
 		ID:     user.ID,
 		Email:  user.Email,
+	}, nil
+}
+
+func (s *service) AuthAPIToken(ctx context.Context, req *models.APITokenAuthRequest) (*models.APITokenAuthResponse, error) {
+	namespace, err := s.store.NamespaceGet(ctx, req.TenantID)
+	if err != nil {
+		return nil, err
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(namespace.Name), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	hasher := sha256.New()
+	if _, err := hasher.Write(hash); err != nil {
+		return nil, err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, models.APITokenAuthClaims{
+		ID:       hex.EncodeToString(hasher.Sum(nil)),
+		TenantID: req.TenantID,
+		AuthClaims: models.AuthClaims{
+			Claims: "apiToken",
+		},
+	})
+
+	tokenStr, err := token.SignedString(s.privKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.APITokenAuthResponse{
+		ID:       hex.EncodeToString(hasher.Sum(nil)),
+		APIToken: tokenStr,
+		TenantID: req.TenantID,
+		ReadOnly: true,
 	}, nil
 }
 

api/store/mocks/store.go
@@ -994,6 +994,103 @@ func (_m *Store) SessionUpdateDeviceUID(ctx context.Context, oldUID models.UID, 
 	return r0
 }
 
+// TokenCreateAPIToken provides a mock function with given fields: ctx, tenantID
+func (_m *Store) TokenCreateAPIToken(ctx context.Context, tenantID string) (*models.Token, error) {
+	ret := _m.Called(ctx, tenantID)
+
+	var r0 *models.Token
+	if rf, ok := ret.Get(0).(func(context.Context, string) *models.Token); ok {
+		r0 = rf(ctx, tenantID)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*models.Token)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, tenantID)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// TokenDeleteAPIToken provides a mock function with given fields: ctx, tenantID, ID
+func (_m *Store) TokenDeleteAPIToken(ctx context.Context, tenantID string, ID string) error {
+	ret := _m.Called(ctx, tenantID, ID)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string, string) error); ok {
+		r0 = rf(ctx, tenantID, ID)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// TokenGetAPIToken provides a mock function with given fields: ctx, tenantID, ID
+func (_m *Store) TokenGetAPIToken(ctx context.Context, tenantID string, ID string) (*models.Token, error) {
+	ret := _m.Called(ctx, tenantID, ID)
+
+	var r0 *models.Token
+	if rf, ok := ret.Get(0).(func(context.Context, string, string) *models.Token); ok {
+		r0 = rf(ctx, tenantID, ID)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*models.Token)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, string, string) error); ok {
+		r1 = rf(ctx, tenantID, ID)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// TokenListAPIToken provides a mock function with given fields: ctx, tenantID
+func (_m *Store) TokenListAPIToken(ctx context.Context, tenantID string) ([]models.Token, error) {
+	ret := _m.Called(ctx, tenantID)
+
+	var r0 []models.Token
+	if rf, ok := ret.Get(0).(func(context.Context, string) []models.Token); ok {
+		r0 = rf(ctx, tenantID)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]models.Token)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, tenantID)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// TokenUpdateAPIToken provides a mock function with given fields: ctx, tenantID, ID, token
+func (_m *Store) TokenUpdateAPIToken(ctx context.Context, tenantID string, ID string, token *models.APITokenUpdate) error {
+	ret := _m.Called(ctx, tenantID, ID, token)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string, string, *models.APITokenUpdate) error); ok {
+		r0 = rf(ctx, tenantID, ID, token)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // UserCreate provides a mock function with given fields: ctx, user
 func (_m *Store) UserCreate(ctx context.Context, user *models.User) error {
 	ret := _m.Called(ctx, user)
@@ -1008,8 +1105,36 @@ func (_m *Store) UserCreate(ctx context.Context, user *models.User) error {
 	return r0
 }
 
+// UserCreateToken provides a mock function with given fields: ctx, token
+func (_m *Store) UserCreateToken(ctx context.Context, token *models.UserTokenRecover) error {
+	ret := _m.Called(ctx, token)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, *models.UserTokenRecover) error); ok {
+		r0 = rf(ctx, token)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // UserDelete provides a mock function with given fields: ctx, ID
 func (_m *Store) UserDelete(ctx context.Context, ID string) error {
+	ret := _m.Called(ctx, ID)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string) error); ok {
+		r0 = rf(ctx, ID)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// UserDeleteTokens provides a mock function with given fields: ctx, ID
+func (_m *Store) UserDeleteTokens(ctx context.Context, ID string) error {
 	ret := _m.Called(ctx, ID)
 
 	var r0 error
@@ -1121,6 +1246,29 @@ func (_m *Store) UserGetByUsername(ctx context.Context, username string) (*model
 	return r0, r1
 }
 
+// UserGetToken provides a mock function with given fields: ctx, ID
+func (_m *Store) UserGetToken(ctx context.Context, ID string) (*models.UserTokenRecover, error) {
+	ret := _m.Called(ctx, ID)
+
+	var r0 *models.UserTokenRecover
+	if rf, ok := ret.Get(0).(func(context.Context, string) *models.UserTokenRecover); ok {
+		r0 = rf(ctx, ID)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*models.UserTokenRecover)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, ID)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // UserList provides a mock function with given fields: ctx, pagination, filters
 func (_m *Store) UserList(ctx context.Context, pagination paginator.Query, filters []models.Filter) ([]models.User, int, error) {
 	ret := _m.Called(ctx, pagination, filters)
@@ -1149,6 +1297,20 @@ func (_m *Store) UserList(ctx context.Context, pagination paginator.Query, filte
 	}
 
 	return r0, r1, r2
+}
+
+// UserUpdateAccountStatus provides a mock function with given fields: ctx, ID
+func (_m *Store) UserUpdateAccountStatus(ctx context.Context, ID string) error {
+	ret := _m.Called(ctx, ID)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string) error); ok {
+		r0 = rf(ctx, ID)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
 }
 
 // UserUpdateData provides a mock function with given fields: ctx, data, ID
@@ -1186,71 +1348,6 @@ func (_m *Store) UserUpdatePassword(ctx context.Context, newPassword string, ID 
 	var r0 error
 	if rf, ok := ret.Get(0).(func(context.Context, string, string) error); ok {
 		r0 = rf(ctx, newPassword, ID)
-	} else {
-		r0 = ret.Error(0)
-	}
-
-	return r0
-}
-
-// UserCreateToken provides a mock function with given fields: ctx, token
-func (_m *Store) UserCreateToken(ctx context.Context, token *models.UserTokenRecover) error {
-	ret := _m.Called(ctx, token)
-
-	var r0 error
-	if rf, ok := ret.Get(0).(func(context.Context, *models.UserTokenRecover) error); ok {
-		r0 = rf(ctx, token)
-	} else {
-		r0 = ret.Error(0)
-	}
-
-	return r0
-}
-
-// UserGetToken provides a mock function with given fields: ctx, ID
-func (_m *Store) UserGetToken(ctx context.Context, ID string) (*models.UserTokenRecover, error) {
-	ret := _m.Called(ctx, ID)
-
-	var r0 *models.UserTokenRecover
-	if rf, ok := ret.Get(0).(func(context.Context, string) *models.UserTokenRecover); ok {
-		r0 = rf(ctx, ID)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*models.UserTokenRecover)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
-		r1 = rf(ctx, ID)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
-// UserDeleteTokens provides a mock function with given fields: ctx, ID
-func (_m *Store) UserDeleteTokens(ctx context.Context, ID string) error {
-	ret := _m.Called(ctx, ID)
-
-	var r0 error
-	if rf, ok := ret.Get(0).(func(context.Context, string) error); ok {
-		r0 = rf(ctx, ID)
-	} else {
-		r0 = ret.Error(0)
-	}
-
-	return r0
-}
-
-// UserUpdateAccountStatus provides a mock function with given fields: ctx, ID
-func (_m *Store) UserUpdateAccountStatus(ctx context.Context, ID string) error {
-	ret := _m.Called(ctx, ID)
-
-	var r0 error
-	if rf, ok := ret.Get(0).(func(context.Context, string) error); ok {
-		r0 = rf(ctx, ID)
 	} else {
 		r0 = ret.Error(0)
 	}